c373c84203a44c99b6d143cbad54434f887b47b6
Manish <121147194+Manish68829@users.noreply.github.com>
2023-05-01 23:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function playSong(file) {
+	var audio = new Audio('sounds/' + file)
+	audio.play()
+}
+
+function stopAllSong() {
+	var songs = document.getElementsByTagName('audio')
+	for (let i = 0; i < songs.length; i++) {
+		songs[i].pause()
+		songs[i].currentTime = 0
+	}
+}
